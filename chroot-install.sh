96f1c583e7bcd3e153f6c496e0f76223bfe1559f
项晓波 <172927583@qq.com>
2018-10-29 16:54:54 +0800

    rank mirrors

---
+++ chroot-install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+user=$1
+password=$2
+
+# rank mirrors
+echo 'Setting up mirrors'
+cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
+sed -i 's/^#Server/Server/' /etc/pacman.d/mirrorlist.backup
+rankmirrors -n 6 /etc/pacman.d/mirrorlist.backup > /etc/pacman.d/mirrorlist
