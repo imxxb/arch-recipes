832b3a0db9d133ef6115bdf35a661d01acab62d0
项晓波 <172927583@qq.com>
2018-10-29 22:17:19 +0800

    Update chroot-install.sh

--- chroot-install.sh
+++ chroot-install.sh
@@ -3,11 +3,6 @@
 user=$1
 password=$2
 
-#rank mirrors
-echo 'Setting up mirrors'
-cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
-sed -i 's/^#Server/Server/' /etc/pacman.d/mirrorlist.backup
-rankmirrors -n 6 /etc/pacman.d/mirrorlist.backup > /etc/pacman.d/mirrorlist
 
 #setup timezone
 echo 'Setting up timezone'
