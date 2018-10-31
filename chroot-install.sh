8fc0183c9a95b22b7625a20c4ae139c9b13a0289
imxxb <imxxb@qq.com>
2018-10-31 17:08:41 +0800

    add fonts

--- chroot-install.sh
+++ chroot-install.sh
@@ -34,6 +34,20 @@ grub-mkconfig -o /boot/grub/grub.cfg
 echo 'Installing Xorg'
 pacman -S --noconfirm xorg xorg-xinit xterm
 
+#install fonts
+pacman -S --noconfirm font-mathematica texlive-core
+pacman -S --noconfirm texlive-fontsextra noto-fonts-emoji
+pacman -S --noconfirm adobe-source-han-sans-otc-fonts 
+pacman -S --noconfirm adobe-source-han-serif-otc-fonts noto-fonts-cjk 
+pacman -S --noconfirm wqy-microhei wqy-microhei-lite 
+pacman -S --noconfirm adobe-source-han-serif-cn-fonts 
+pacman -S --noconfirm adobe-source-han-serif-tw-fonts 
+pacman -S --noconfirm adobe-source-han-sans-cn-fonts 
+pacman -S --noconfirm adobe-source-han-sans-tw-fonts 
+pacman -S --noconfirm wqy-zenhei wqy-bitmapfont 
+pacman -S --noconfirm ttf-arphic-ukai ttf-arphic-uming 
+pacman -S --noconfirm opendesktop-fonts  ttf-hannom
+
 #install rake
 echo 'Installing development utils'
 pacman -S --noconfirm ruby-rake git vim dhclient sudo
