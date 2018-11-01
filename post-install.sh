722b7c43a641cfd97b05d4ff4bda4d476b0ced2b
项晓波 <172927583@qq.com>
2018-11-01 09:12:11 +0800

    add desktop config from spartan-arch

--- post-install.sh
+++ post-install.sh
@@ -6,8 +6,8 @@ sudo dhcpcd
 
 cd
 head -n -5 /etc/X11/xinit/xinitrc > ~/.xinitrc
-# echo 'exec VBoxClient --clipboard -d &' >> ~/.xinitrc
-# echo 'exec VBoxClient --display -d &' >> ~/.xinitrc
+echo 'exec VBoxClient --clipboard -d &' >> ~/.xinitrc
+echo 'exec VBoxClient --display -d &' >> ~/.xinitrc
 echo 'exec i3 &' >> ~/.xinitrc
 echo 'exec nitrogen --restore &' >> ~/.xinitrc
 echo 'exec emacs' >> ~/.xinitrc
@@ -18,3 +18,29 @@ echo 'XTerm*background:black' > ~/.Xdefaults
 echo 'XTerm*foreground:white' >> ~/.Xdefaults
 echo 'UXTerm*background:black' >> ~/.Xdefaults
 echo 'UXTerm*foreground:white' >> ~/.Xdefaults
+
+
+#i3status
+if [ ! -d ~/.config ]; then
+    mkdir ~/.config
+fi
+mkdir ~/.config/i3status
+cp /etc/i3status.conf ~/.config/i3status/config
+sed -i 's/^order += "ipv6"/#order += "ipv6"/' ~/.config/i3status/config
+sed -i 's/^order += "run_watch VPN"/#order += "run_watch VPN"/' ~/.config/i3status/config
+sed -i 's/^order += "wireless _first_"/#order += "wireless _first_"/' ~/.config/i3status/config
+sed -i 's/^order += "battery 0"/#order += "battery 0"/' ~/.config/i3status/config
+
+
+# wallpaper setup
+cd
+mkdir Pictures
+cd Pictures
+wget https://initiate.alphacoders.com/download/picture/212714/jpg/228334383932218 -O wallpaper.jpg
+cd ~/.config/
+mkdir nitrogen
+cd nitrogen
+echo '[xin_-1]' > bg-saved.cfg
+echo "file=/home/$(whoami)/Pictures/wallpaper.jpg" >> bg-saved.cfg
+echo 'mode=0' >> bg-saved.cfg
+echo 'bgcolor=#000000' >> bg-saved.cfg
